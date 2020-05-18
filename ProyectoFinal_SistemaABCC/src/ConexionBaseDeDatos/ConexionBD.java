b42f104014dbd97a7ed9b2f2c0bda3ddf1f2be6e
Felipe Marquez <flype360360@gmail.com>
2020-05-18 11:54:24 -0500

    Se han creado los paquetes necesarios y una carpeta de recursos

---
+++ ProyectoFinal_SistemaABCC/src/ConexionBaseDeDatos/ConexionBD.java
@@ -0,0 +1,5 @@
+package ConexionBaseDeDatos;
+
+public class ConexionBD {
+
+}
---
+++ ProyectoFinal_SistemaABCC/src/Vista/VentanaInicio.java
@@ -0,0 +1,10 @@
+package Vista;
+
+public class VentanaInicio {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
