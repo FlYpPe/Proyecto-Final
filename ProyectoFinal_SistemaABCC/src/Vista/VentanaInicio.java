8ab836c9c7870b69a80109c30c6ae54fb42ab0fe
Felipe Marquez <flype360360@gmail.com>
2020-05-25 10:12:40 -0500

    Se anadio el Jframe principal

--- ProyectoFinal_SistemaABCC/src/Vista/VentanaInicio.java
+++ ProyectoFinal_SistemaABCC/src/Vista/VentanaInicio.java
@@ -1,9 +1,42 @@
 package Vista;
 
+import java.awt.Color;
+
+import javax.swing.JFrame;
+import javax.swing.SwingUtilities;
+
+class Gui extends JFrame{
+	
+	public Gui() {
+		getContentPane().setLayout(null);
+		setDefaultCloseOperation(EXIT_ON_CLOSE);
+		setTitle("Sistema ABCC");
+
+		setSize(500, 460);
+		setLocationRelativeTo(null);
+		setVisible(true);
+		getContentPane().setBackground(Color.WHITE);
+		
+		
+		
+	}
+	
+}
+
 public class VentanaInicio {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
+		SwingUtilities.invokeLater(new Runnable() {
+
+			@Override
+			public void run() {
+				new Gui();
+
+			}
+		});
+		
+		
 
 	}
 
