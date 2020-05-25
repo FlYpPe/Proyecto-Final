0fa3fa8a9b9e49b3092aa23ebdba4f80a3c747a1
Felipe Marquez <flype360360@gmail.com>
2020-05-25 16:46:40 -0500

    Se ha creado un metodo para retornar Jtables, solicitando los datos a mysql

--- ProyectoFinal_SistemaABCC/src/Controlador/Controlador.java
+++ ProyectoFinal_SistemaABCC/src/Controlador/Controlador.java
@@ -5,78 +5,82 @@ import java.sql.ResultSetMetaData;
 import java.sql.SQLException;
 import java.util.ArrayList;
 
+import javax.swing.JTable;
 import javax.swing.table.AbstractTableModel;
 
 import ConexionBaseDeDatos.*;
 
-/*
 public class Controlador {
-
+/*
 	public boolean agregarAlumno(Alumno a) {
-		String sql = "insert into Alumnos Values('"+a.getNumControl()+"','"+a.getPrimerAp()+"','"+a.getNombre()+"','"+a.getSegundoAp()+"',"+a.getEdad()+","+a.getSemestre()+",'"+a.getCarrera()+ 	"')";
-		
+		String sql = "insert into Alumnos Values('" + a.getNumControl() + "','" + a.getPrimerAp() + "','"
+				+ a.getNombre() + "','" + a.getSegundoAp() + "'," + a.getEdad() + "," + a.getSemestre() + ",'"
+				+ a.getCarrera() + "')";
+
 		return new ConexionBD().ejInstr(sql);
-		
+
 	}
-	
+
 	public boolean eliminarAlumno(String numControl) {
-		
-		String sql = "Delete From alumnos where num_control = '"+numControl+"'";
-		boolean res =  new ConexionBD().ejInstr(sql);
-		
+
+		String sql = "Delete From alumnos where num_control = '" + numControl + "'";
+		boolean res = new ConexionBD().ejInstr(sql);
+
 		return false;
-		
+
 	}
-	
+
 	public boolean modificarAlumno(Alumno a) {
-		
-		String sql = "Update Alumnos Set Nombre_Alumno= '"+a.getNombre()+"'"
-				+ ", Primer_Ap_Alumno = '"+ a.getPrimerAp()+"'"
-				+ ", Segundo_Ap_Alumno = '"+a.getSegundoAp()+"'"
-				+ ", Edad_Alumno = "+a.getEdad()+""
-				+ ", Semestre = "+a.getSemestre()+""
-				+ ", Carrera = '"+a.getCarrera()+ "' WHERE Num_Control='"+a.getNumControl()+"'";
-		
+
+		String sql = "Update Alumnos Set Nombre_Alumno= '" + a.getNombre() + "'" + ", Primer_Ap_Alumno = '"
+				+ a.getPrimerAp() + "'" + ", Segundo_Ap_Alumno = '" + a.getSegundoAp() + "'" + ", Edad_Alumno = "
+				+ a.getEdad() + "" + ", Semestre = " + a.getSemestre() + "" + ", Carrera = '" + a.getCarrera()
+				+ "' WHERE Num_Control='" + a.getNumControl() + "'";
+
 //		Nombre_Alumno | Primer_Ap_Alumno | Segundo_Ap_Alumno | Edad_Alumno | Semestre | Carrera
-		
-		boolean res =  new ConexionBD().ejInstr(sql);
+
+		boolean res = new ConexionBD().ejInstr(sql);
 		return res;
-		
+
 	}
-	
-	
-public Alumno buscarAlumno(String numControl) {
-		
-		
-		String sql = "SELECT * FROM Alumnos WHERE Num_Control = '"+numControl+"'";
-		
+
+	public Alumno buscarAlumno(String numControl) {
+
+		String sql = "SELECT * FROM Alumnos WHERE Num_Control = '" + numControl + "'";
+
 		ResultSet res = new ConexionBD().ejecutarConsulta(sql);
-		
+
 		try {
 			res.last();
-			
-			return new Alumno(res.getString("Num_Control"),res.getString(2),res.getString(3),res.getString(4),res.getByte(5),res.getByte(5),res.getString(6));
-			
+
+			return new Alumno(res.getString("Num_Control"), res.getString(2), res.getString(3), res.getString(4),
+					res.getByte(5), res.getByte(5), res.getString(6));
+
 		} catch (SQLException e) {
 			// TODO Auto-generated catch block
-			
+
 			return null;
 		}
-		
-		
-		
-		
-	}
-	
-}*/
 
-class crearTabla extends AbstractTableModel{
+	}
+*/
+public JTable retornarTabla(String nTabla) {
+	
+	CrearTabla modelo = new CrearTabla(new ConexionBD().ejecutarConsulta("SELECT * FROM " + nTabla));
+	
+	return new JTable(modelo);
+	
+}
+
+}
+
+class CrearTabla extends AbstractTableModel {
 
 	private ResultSet rsRegistros;
 	private ResultSetMetaData metaData;
-	
-	public crearTabla(ResultSet unResultSet) {
-		
+
+	public CrearTabla(ResultSet unResultSet) {
+
 		this.rsRegistros = unResultSet;
 		try {
 			this.metaData = this.rsRegistros.getMetaData();
@@ -85,26 +89,23 @@ class crearTabla extends AbstractTableModel{
 			e.printStackTrace();
 		}
 	}
-	
+
 	@Override
 	public int getColumnCount() {
-		
-		
+
 		try {
 			return metaData.getColumnCount();
 		} catch (SQLException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
-		
+
 		return 0;
 	}
 
 	@Override
 	public int getRowCount() {
-		
-		
-		
+
 		try {
 			rsRegistros.last();
 			return rsRegistros.getRow();
@@ -113,12 +114,12 @@ class crearTabla extends AbstractTableModel{
 			e.printStackTrace();
 		}
 		return 0;
-		
+
 	}
 
 	@Override
 	public Object getValueAt(int arg0, int arg1) {
-		
+
 		try {
 			rsRegistros.absolute(arg0 + 1);
 			return rsRegistros.getObject(arg1 + 1);
@@ -127,15 +128,18 @@ class crearTabla extends AbstractTableModel{
 			e.printStackTrace();
 		}
 		return null;
-		
-		
-		
-		
+
 	}
-	
-	
-	
-	
-	
+
+	public String getColumnName(int c) {
+		
+		try {
+			return metaData.getColumnName(c + 1);
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		return null;
+	}
 	
 }
