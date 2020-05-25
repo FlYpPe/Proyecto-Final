93196c576d61636df54b44872109134c00ae8bf4
Felipe Marquez <flype360360@gmail.com>
2020-05-25 16:47:40 -0500

    Se ha heclo la implementacion de el metodo de controlador para mostrar las tablas en el Frame

--- ProyectoFinal_SistemaABCC/src/Vista/VentanaInicio.java
+++ ProyectoFinal_SistemaABCC/src/Vista/VentanaInicio.java
@@ -1,11 +1,21 @@
 package Vista;
 
+import java.awt.BorderLayout;
 import java.awt.Color;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
+import javax.swing.JButton;
 import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.JScrollPane;
+import javax.swing.JTable;
 import javax.swing.SwingUtilities;	
+import Controlador.*;
 
 class Gui extends JFrame{
+	
+	JTable tabla;
 	
 	public Gui() {
 		getContentPane().setLayout(null);
@@ -17,7 +27,32 @@ class Gui extends JFrame{
 		setVisible(true);
 		getContentPane().setBackground(Color.WHITE);
 		
+		/*
+		JPanel panel = new JPanel();
+		panel.setLayout(new BorderLayout());
+		panel .setBounds(10,10, 100,100);
+		add(panel);
+		*/
+		JButton b1 = new JButton();
+		b1.setBounds(10,120, 30,15);
+		add(b1);
 		
+		b1.addActionListener(new ActionListener() {
+			
+			@Override
+			public void actionPerformed(ActionEvent arg0) {
+				
+				tabla =  new Controlador().retornarTabla("Ayuntamiento");
+				
+				
+				JScrollPane panelsc = new JScrollPane(tabla);
+				panelsc.setBounds(10,10,400, 200);
+				add(panelsc);
+				
+				
+				
+			}
+		});
 		
 	}
 	
