f6df46335e0341ae78b642768e8f5070476d1a82
Felipe Marquez <flype360360@gmail.com>
2020-05-26 16:07:56 -0500

    Se ha creado el menu bar y los menu items, ademas se han agregado al frame principal

--- ProyectoFinal_SistemaABCC/src/Vista/VentanaInicio.java
+++ ProyectoFinal_SistemaABCC/src/Vista/VentanaInicio.java
@@ -7,6 +7,9 @@ import java.awt.event.ActionListener;
 
 import javax.swing.JButton;
 import javax.swing.JFrame;
+import javax.swing.JMenu;
+import javax.swing.JMenuBar;
+import javax.swing.JMenuItem;
 import javax.swing.JPanel;
 import javax.swing.JScrollPane;
 import javax.swing.JTable;
@@ -16,6 +19,9 @@ import Controlador.*;
 class Gui extends JFrame{
 	
 	JTable tabla;
+	JMenuBar menuBar;
+	JMenu menuInicio;
+	JMenuItem menuAltas, menuCambio, menuBajas, menuConsultas;
 	
 	public Gui() {
 		getContentPane().setLayout(null);
@@ -27,12 +33,32 @@ class Gui extends JFrame{
 		setVisible(true);
 		getContentPane().setBackground(Color.WHITE);
 		
+		menuBar = new JMenuBar();
+		menuInicio = new JMenu("Ver");
+		
+		menuAltas = new JMenuItem("Altas");
+		menuCambio = new JMenuItem("Cambio");
+		menuBajas = new JMenuItem("Bajas");
+		menuConsultas = new JMenuItem("Consultas");
+		
+		menuInicio.add(menuAltas);
+		menuInicio.add(menuCambio);
+		menuInicio.add(menuBajas);
+		menuInicio.add(menuConsultas);
+		menuBar.add(menuInicio);
+		setJMenuBar(menuBar);
+		
+		
+		
 		/*
 		JPanel panel = new JPanel();
 		panel.setLayout(new BorderLayout());
 		panel .setBounds(10,10, 100,100);
 		add(panel);
 		*/
+		
+		
+		
 		JButton b1 = new JButton();
 		b1.setBounds(10,120, 30,15);
 		add(b1);
