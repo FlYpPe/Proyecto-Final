ff5280ae2b65599f62eef125a4c71c49cd22fac6
Felipe Marquez <flype360360@gmail.com>
2020-05-28 19:22:38 -0500

    Se anadio una clase que implementa Runnable para las consultas por medio de hilos y un metodo en la clase controlador para hacer los registros en cualquier tabla

--- ProyectoFinal_SistemaABCC/src/Controlador/Controlador.java
+++ ProyectoFinal_SistemaABCC/src/Controlador/Controlador.java
@@ -11,67 +11,93 @@ import javax.swing.table.AbstractTableModel;
 import ConexionBaseDeDatos.*;
 
 public class Controlador {
-/*
-	public boolean agregarAlumno(Alumno a) {
-		String sql = "insert into Alumnos Values('" + a.getNumControl() + "','" + a.getPrimerAp() + "','"
-				+ a.getNombre() + "','" + a.getSegundoAp() + "'," + a.getEdad() + "," + a.getSemestre() + ",'"
-				+ a.getCarrera() + "')";
-
-		return new ConexionBD().ejInstr(sql);
-
-	}
-
-	public boolean eliminarAlumno(String numControl) {
-
-		String sql = "Delete From alumnos where num_control = '" + numControl + "'";
-		boolean res = new ConexionBD().ejInstr(sql);
-
-		return false;
-
-	}
-
-	public boolean modificarAlumno(Alumno a) {
-
-		String sql = "Update Alumnos Set Nombre_Alumno= '" + a.getNombre() + "'" + ", Primer_Ap_Alumno = '"
-				+ a.getPrimerAp() + "'" + ", Segundo_Ap_Alumno = '" + a.getSegundoAp() + "'" + ", Edad_Alumno = "
-				+ a.getEdad() + "" + ", Semestre = " + a.getSemestre() + "" + ", Carrera = '" + a.getCarrera()
-				+ "' WHERE Num_Control='" + a.getNumControl() + "'";
-
-//		Nombre_Alumno | Primer_Ap_Alumno | Segundo_Ap_Alumno | Edad_Alumno | Semestre | Carrera
-
-		boolean res = new ConexionBD().ejInstr(sql);
-		return res;
-
-	}
-
-	public Alumno buscarAlumno(String numControl) {
-
-		String sql = "SELECT * FROM Alumnos WHERE Num_Control = '" + numControl + "'";
-
-		ResultSet res = new ConexionBD().ejecutarConsulta(sql);
-
-		try {
-			res.last();
-
-			return new Alumno(res.getString("Num_Control"), res.getString(2), res.getString(3), res.getString(4),
-					res.getByte(5), res.getByte(5), res.getString(6));
-
-		} catch (SQLException e) {
-			// TODO Auto-generated catch block
-
-			return null;
-		}
-
-	}
-*/
-public JTable retornarTabla(String nTabla) {
 	
-	CrearTabla modelo = new CrearTabla(new ConexionBD().ejecutarConsulta("SELECT * FROM " + nTabla));
 	
-	return new JTable(modelo);
 	
+	 public boolean agregarRegistro(String tabla, String values) { 
+		 
+		 String sql ="insert into"+ tabla + values ;
+			  
+			  return new ConexionBD().ejInstr(sql);
+			  
+			  }
+	
+	
+	
+	/*
+	 * public boolean agregarAlumno(Alumno a) { String sql =
+	 * "insert into Alumnos Values('" + a.getNumControl() + "','" + a.getPrimerAp()
+	 * + "','" + a.getNombre() + "','" + a.getSegundoAp() + "'," + a.getEdad() + ","
+	 * + a.getSemestre() + ",'" + a.getCarrera() + "')";
+	 * 
+	 * return new ConexionBD().ejInstr(sql);
+	 * 
+	 * }
+	 * 
+	 * public boolean eliminarAlumno(String numControl) {
+	 * 
+	 * String sql = "Delete From alumnos where num_control = '" + numControl + "'";
+	 * boolean res = new ConexionBD().ejInstr(sql);
+	 * 
+	 * return false;
+	 * 
+	 * }
+	 * 
+	 * public boolean modificarAlumno(Alumno a) {
+	 * 
+	 * String sql = "Update Alumnos Set Nombre_Alumno= '" + a.getNombre() + "'" +
+	 * ", Primer_Ap_Alumno = '" + a.getPrimerAp() + "'" + ", Segundo_Ap_Alumno = '"
+	 * + a.getSegundoAp() + "'" + ", Edad_Alumno = " + a.getEdad() + "" +
+	 * ", Semestre = " + a.getSemestre() + "" + ", Carrera = '" + a.getCarrera() +
+	 * "' WHERE Num_Control='" + a.getNumControl() + "'";
+	 * 
+	 * // Nombre_Alumno | Primer_Ap_Alumno | Segundo_Ap_Alumno | Edad_Alumno |
+	 * Semestre | Carrera
+	 * 
+	 * boolean res = new ConexionBD().ejInstr(sql); return res;
+	 * 
+	 * }
+	 * 
+	 * public Alumno buscarAlumno(String numControl) {
+	 * 
+	 * String sql = "SELECT * FROM Alumnos WHERE Num_Control = '" + numControl +
+	 * "'";
+	 * 
+	 * ResultSet res = new ConexionBD().ejecutarConsulta(sql);
+	 * 
+	 * try { res.last();
+	 * 
+	 * return new Alumno(res.getString("Num_Control"), res.getString(2),
+	 * res.getString(3), res.getString(4), res.getByte(5), res.getByte(5),
+	 * res.getString(6));
+	 * 
+	 * } catch (SQLException e) { // TODO Auto-generated catch block
+	 * 
+	 * return null; }
+	 * 
+	 * }
+	 */
+	public JTable retornarTabla(String nTabla) {
+
+		CrearTabla modelo = new CrearTabla(new ConexionBD().ejecutarConsulta("SELECT * FROM " + nTabla));
+
+		return new JTable(modelo);
+
+	}
+
 }
 
+
+
+class consulta implements Runnable{
+
+	@Override
+	public void run() {
+		
+		
+		
+	}
+	
 }
 
 class CrearTabla extends AbstractTableModel {
@@ -132,7 +158,7 @@ class CrearTabla extends AbstractTableModel {
 	}
 
 	public String getColumnName(int c) {
-		
+
 		try {
 			return metaData.getColumnName(c + 1);
 		} catch (SQLException e) {
@@ -141,5 +167,5 @@ class CrearTabla extends AbstractTableModel {
 		}
 		return null;
 	}
-	
+
 }
